ace37d71d6090f70b02dfd226c82f1ab880fee21
larissa557197 <larissampmuniz@gmail.com>
2025-10-21 00:00:43 -0300

    feat(domain): add pagination support and unify repository pattern across entities

--- VisionHive.Domain/Repositories/IMotoRepository.cs
+++ VisionHive.Domain/Repositories/IMotoRepository.cs
@@ -1,10 +1,4 @@
 using VisionHive.Domain.Pagination;
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using VisionHive.Application.DTO.Request;
-using VisionHive.Application.DTO.Response;
 using VisionHive.Domain.Entities;
 
 namespace VisionHive.Domain.Repositories
@@ -13,10 +7,10 @@ namespace VisionHive.Domain.Repositories
 
     public interface IMotoRepository
     {
-        Task<MotoResponse> AddMotoAsync(MotoRequest request, CancellationToken ct = default);
-        Task<MotoResponse?> GetByIdAsync(Guid id, CancellationToken ct = default);
-        Task<PageResult<MotoResponse>> GetPaginationAsync(PaginatedRequest request, CancellationToken ct = default);
-        Task<bool> UpdateAsync(Guid id, MotoRequest request, CancellationToken ct = default);
+        Task<Moto> AddAsync(Moto moto, CancellationToken ct = default);
+        Task<PageResult<Moto>> GetPaginationAsync(int page, int pageSize, string? search, CancellationToken ct = default);
+        Task<Moto?> GetByIdAsync(Guid id, CancellationToken ct = default);
+        Task<bool> UpdateAsync(Moto moto, CancellationToken ct = default);
         Task<bool> DeleteAsync(Guid id, CancellationToken ct = default);
     }
 }
