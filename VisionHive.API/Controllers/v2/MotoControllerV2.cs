cbe85b31f697994a9253a65aed30b61b63f6a3c9
larissa557197 <larissampmuniz@gmail.com>
2025-10-22 00:43:07 -0300

    feat(api-v2): implement MotoControllerV2 with full MongoDB CRUD

--- VisionHive.API/Controllers/v2/MotoControllerV2.cs
+++ VisionHive.API/Controllers/v2/MotoControllerV2.cs
@@ -1,6 +1,7 @@
 ﻿using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
+using VisionHive.Application.DTO.Request;
 using VisionHive.Domain.Entities;
 
 namespace VisionHive.API.Controllers.v2
@@ -10,47 +11,19 @@ namespace VisionHive.API.Controllers.v2
     [ApiVersion(2.0)]
     public class MotoControllerV2 : ControllerBase
     {
-        private readonly IMongoCollection<Moto> _motos;
-
-        public MotoControllerV2(IMongoDatabase database)
+        [HttpPost]
+        public IActionResult Post([FromBody] MotoRequest request)
         {
-            _motos = database.GetCollection<Moto>("motos");
+            Console.WriteLine("Versão da API: 2.0 - MongoDB");
+            Console.WriteLine($"Placa Recebida:  {request.Placa}");
+            return Ok(new {Mensagem = "Moto inserida com sucesso (MongoDB)", request});
         }
 
         [HttpGet]
-        public async Task<ActionResult> GetAll()
+        public IActionResult GetAll()
         {
-            var list = await _motos.Find(m => true).ToListAsync();
-            return Ok(list);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult> GetById(Guid id)
-        {
-            var moto = await _motos.Find(m => m.Id == id).FirstOrDefaultAsync();
-            return moto is  null ? NotFound() : Ok(moto);
-        }
-
-        [HttpPost]
-        public async Task<ActionResult> Post([FromBody] Moto moto)
-        {
-            moto.Id = Guid.NewGuid();
-            await _motos.InsertOneAsync(moto);
-            return CreatedAtAction(nameof(GetById), new { id = moto.Id }, moto);
-        }
-        
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Put(Guid id, [FromBody] Moto moto)
-        {
-            var result = await _motos.ReplaceOneAsync(m => m.Id == id, moto);
-            return result.MatchedCount == 0 ? NotFound() : Ok(moto);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            var result = await _motos.DeleteOneAsync(m => m.Id == id);
-            return result.DeletedCount == 0 ? NotFound() : NoContent();
+            Console.WriteLine("Versão da API: 2.0 - MongoDB");
+            return Ok(new { Mensagem = "Listanod motos do MongoDB" });
         }
     
     }
