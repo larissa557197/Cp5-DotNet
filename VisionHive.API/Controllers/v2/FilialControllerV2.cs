116b894524e0f2dd1a6573833d36f42ac0e3875a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-22 08:29:40 -0300

    refactor(api-v2):implement PatioControllerV2 with full MongoDB CRUD

--- VisionHive.API/Controllers/v2/FilialControllerV2.cs
+++ VisionHive.API/Controllers/v2/FilialControllerV2.cs
@@ -2,6 +2,7 @@
 using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
+using VisionHive.Application.DTO.Request;
 using VisionHive.Domain.Entities;
 
 namespace VisionHive.API.Controllers.v2
@@ -11,50 +12,22 @@ namespace VisionHive.API.Controllers.v2
     [ApiVersion(2.0)]
     public class FilialControllerV2: ControllerBase
     {
-        
-        private readonly IMongoCollection<Filial> _filiais;
 
-        public FilialControllerV2(IMongoDatabase database)
+        [HttpPost]
+        public IActionResult Post([FromBody] FilialRequest request)
         {
-            _filiais = database.GetCollection<Filial>("filiais");
+            Console.WriteLine("Versão da API: 2.0 - MongoDB");
+            Console.WriteLine($"Placa Recebida:  {request.Nome}");
+            return Ok(new { Mensagem = "Filial inserida com sucesso (MongoDB)", request });
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public IActionResult GetAll()
         {
-            var list = await _filiais.Find(_ => true).ToListAsync();
-            return Ok(list);
+            Console.WriteLine("Versão da API: 2.0 - MongoDB");
+            return Ok(new { Mensagem = "Listando filiais do MongoDB" });
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(Guid id)
-        {
-            var filial = await _filiais.Find(f => f.Id == id).FirstOrDefaultAsync();
-            return filial is null ? NotFound() : Ok(filial);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody] Filial filial)
-        {
-            filial.Id = Guid.NewGuid();
-            await _filiais.InsertOneAsync(filial);
-            return CreatedAtAction(nameof(GetById), new { id = filial.Id }, filial);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Put(Guid id, [FromBody] Filial filial)
-        {
-            var result = await _filiais.ReplaceOneAsync(f => f.Id == id, filial);
-            return result.MatchedCount == 0 ? NotFound() : Ok(filial);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            var result = await _filiais.DeleteOneAsync(f => f.Id == id);
-            return result.DeletedCount == 0 ? NotFound() : NoContent();
-        }
-    
     }
 }
 
