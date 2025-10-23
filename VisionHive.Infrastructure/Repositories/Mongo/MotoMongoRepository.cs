22836afc5610f31279691219b4ed76813a3cd714
larissa557197 <larissampmuniz@gmail.com>
2025-10-23 19:28:54 -0300

    feat(moto): implemented MotoMongoRepository with CRUD base methods
    Created MotoMongoRepository to handle MongoDB persistence for Moto entity, including serializer registration for Guid compatibility and basic async methods (CreateAsync, GetAllAsync).

---
+++ VisionHive.Infrastructure/Repositories/Mongo/MotoMongoRepository.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using VisionHive.Domain.Entities;
+
+namespace VisionHive.Infrastructure.Repositories.Mongo;
+
+public class MotoMongoRepository
+{
+    private readonly IMongoCollection<Moto> _collection;
+
+    public MotoMongoRepository(IMongoDatabase database)
+    {
+        // Registro do serializer compatível com qualquer versão
+        try
+        {
+            BsonSerializer.RegisterSerializer(typeof(Guid), new GuidSerializer(GuidRepresentation.Standard));
+        }
+        catch
+        {
+            // ignora caso já tenha sido registrado
+        }
+
+        _collection = database.GetCollection<Moto>("Motos");
+    }
+
+
+    public async Task<Moto> CreateAsync(Moto moto)
+    {
+        await _collection.InsertOneAsync(moto);
+        return moto;
+    }
+
+
+    public async Task<List<Moto>> GetAllAsync()
+    {
+        return await _collection.Find(_ => true).ToListAsync();
+    }
+}
